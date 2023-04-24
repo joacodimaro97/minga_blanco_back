d2212fc1389cfec89437f04c79b0b277bc3105cc
joacodimaro97 <joacodimaro97@gmail.com>
2023-04-24 11:35:36 -0300

    b-08 (#7)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 import express from 'express'
+import userRouter from './users.js'
+import authorRouter from './authors.js'
+import categoriesRoutes from './categories.js'
+import chapterRouter from './chapters.js'
+import companiesRouter from './companies.js'
+import mangasRouter from './mangas.js'
 const router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'MINGA API',
+    subtitle: 'endpoints of minga'
 });
+});
+
+router.use('/users', userRouter)
+router.use('/authors', authorRouter)
+router.use('/categories', categoriesRoutes)
+router.use('/chapters', chapterRouter)
+router.use('/companies', companiesRouter)
+router.use('/mangas', mangasRouter)
 
 export default router;
 
