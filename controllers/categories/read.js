6266225743a4ba89543f1b4a8278fb2f5e45f619
joacodimaro97 <joacodimaro97@gmail.com>
2023-04-24 11:52:51 -0300

    b-09 (#8)

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -1,7 +1,23 @@
-let read = (req,res,next)=>res.status(200).render('index',{
-    title: '/CATEGORIES',
-    subtitle: 'endpoints of categories'
-})
+import Category from './../../models/Category.js'
 
+
+let read = async(req,res,next)=>{
+    try{
+        let all = await Category.find()
+        return res.status(200)
+        .json({
+            categories: all
+        })
+    }catch(error) {
+        console.log(error)
+        return res.status(400)
+        .json({
+            error: 'ha ocurrido un error'
+        })
+    }
+    
+    
+
+}
 
 export default read
