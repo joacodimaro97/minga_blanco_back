f6cd97147f5fd474e6af7dd1f9c704efd290e090
joacodimaro97 <joacodimaro97@gmail.com>
2023-04-24 13:13:38 -0300

    02 (#9)

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import express from 'express'
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors'
 
 import indexRouter from './routes/index.js';
 
@@ -19,6 +20,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
