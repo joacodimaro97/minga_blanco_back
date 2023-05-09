14c778458c2202d5b83c4a89807dc1090f58c9aa
CrisDev <alvarezmartinez132@gmail.com>
2023-05-09 00:29:08 -0500

    9/5/23_00:29

--- controllers/mangas/get_mangas.js
+++ controllers/mangas/get_mangas.js
@@ -2,8 +2,12 @@ import Manga from '../../models/Manga.js'
 let getMangas = async(req,res,next) => {
     try {
         let queries = {}
-        req.query.title ? queries.title = new RegExp(req.query.title.trim(), 'i') : ''
-        queries.category_id = req.query.category_id ?? ''
+        if(req.query.title){
+            queries.title = new RegExp(req.query.title.trim(), 'i')
+        }
+        if(req.query.category_id){
+            queries.category_id = req.query.category_id.split(',')
+        }
         console.log(queries)
         let all = await Manga.find(queries)
         return res.status(200).json({
