5aeff8138381a46e0e74e58c73aa911f50a83d2d
CrisDev <alvarezmartinez132@gmail.com>
2023-05-09 23:25:36 -0500

    9/5/23_23:25

--- controllers/mangas/get_mangas.js
+++ controllers/mangas/get_mangas.js
@@ -2,14 +2,26 @@ import Manga from '../../models/Manga.js'
 let getMangas = async(req,res,next) => {
     try {
         let queries = {}
+        let sort = {}
+        let pagination = {
+            limit: 6,
+            page: 1
+        }
         if(req.query.title){
             queries.title = new RegExp(req.query.title.trim(), 'i')
         }
         if(req.query.category_id){
             queries.category_id = req.query.category_id.split(',')
         }
+        sort.title = req.query.order ?? 1
+        pagination.limit = req.query.limit ?? 6
+        pagination.page = req.query.page ?? 1
         console.log(queries)
-        let all = await Manga.find(queries)
+        let all = await Manga
+                    .find(queries).select("title cover_photo")
+                    .sort(sort)
+                    .skip(pagination.page > 0 ? (pagination.page-1)*pagination.limit : 0)
+                    .limit(pagination.limit > 0 ? pagination.limit : 0)
         return res.status(200).json({
             success: true,
             response: all
