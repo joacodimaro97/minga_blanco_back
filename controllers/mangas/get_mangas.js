9a276d31c933cd79c364004c7dd82811ecb2d497
Cristian Álvarez <94159947+CristianAlvarezMartinez@users.noreply.github.com>
2023-05-31 21:33:03 -0500

    Update get_mangas.js

--- controllers/mangas/get_mangas.js
+++ controllers/mangas/get_mangas.js
@@ -1,12 +1,7 @@
-import Manga from '../../models/Manga.js'
 let getMangas = async(req,res,next) => {
     try {
         let queries = {}
         let sort = {}
-        let pagination = {
-            limit: 6,
-            page: 1
-        }
         if(req.query.title){
             queries.title = new RegExp(req.query.title.trim(), 'i')
         }
@@ -14,14 +9,10 @@ let getMangas = async(req,res,next) => {
             queries.category_id = req.query.category_id.split(',')
         }
         sort.title = req.query.order ?? 1
-        pagination.limit = req.query.limit ?? 6
-        pagination.page = req.query.page ?? 1
         console.log(queries)
         let all = await Manga
                     .find(queries).select("title cover_photo author_id")
                     .sort(sort)
-                    .skip(pagination.page > 0 ? (pagination.page-1)*pagination.limit : 0)
-                    .limit(pagination.limit > 0 ? pagination.limit : 0)
         return res.status(200).json({
             success: true,
             response: all
